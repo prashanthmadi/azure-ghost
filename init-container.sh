dc66779685ecca29019a771ea1ef73a1f5b0bb7d
prashanthmadi <prashanthrmadi@gmail.com>
2017-08-22 17:01:33 -0500

    :bug: adding ghost content config path

--- init-container.sh
+++ init-container.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 service ssh start
 gosu node ghost config --db="mysql" --dbhost="$DB_HOST" --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --url="$GHOST_URL"
+gosu node ghost config paths.contentPath "$GHOST_CONTENT"
 chown -R node "$GHOST_CONTENT"
+sh /bin/migrate_util.sh
 /usr/bin/supervisord
