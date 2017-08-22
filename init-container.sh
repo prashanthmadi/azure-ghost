f7b5cc8f9ae8c887a13c9146784561ecde03884b
prashanthmadi <prashanthrmadi@gmail.com>
2017-08-22 14:58:24 -0500

    :fallen_leaf: fix ghost config issue

--- init-container.sh
+++ init-container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 service ssh start
-gosu node ghost config --db "mysql" --pname "ghost" --dbhost "$DB_HOST" --dbname "$DB_NAME" --dbuser "$DB_USER" --dbpass "$DB_PASS" paths.contentPath "$GHOST_CONTENT"
+gosu node ghost config --db="mysql" --dbhost="$DB_HOST" --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --url="$GHOST_URL"
 chown -R node "$GHOST_CONTENT"
 /usr/bin/supervisord
