6dc23f84d7922c33d359598c2cf164e8b3ba6caa
Prashanth Madi <prashanthrmadi@gmail.com>
2017-08-22 23:32:24 -0500

    :back: limiting connections to 4 will revert later
    this is for testing purpose, will revert soon

--- init-container.sh
+++ init-container.sh
@@ -2,6 +2,8 @@
 service ssh start
 gosu node ghost config --db="mysql" --dbhost="$DB_HOST" --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --url="$GHOST_URL"
 gosu node ghost config paths.contentPath "$GHOST_CONTENT"
+gosu node ghost config database.pool.max 4
+gosu node ghost config database.pool.min 1
 chown -R node "$GHOST_CONTENT"
 echo "current directory"
 echo $PWD
