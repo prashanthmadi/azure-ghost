cd232135c7be3bb0fd0f361c1c134b99d39583b8
prashanthmadi <prashanthrmadi@gmail.com>
2017-08-24 16:47:03 -0500

    changing port to 0.0.0.0

--- init-container.sh
+++ init-container.sh
@@ -2,5 +2,6 @@
 service ssh start
 ghost config --db="mysql" --dbhost="$DB_HOST" --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --url="$GHOST_URL"
 ghost config paths.contentPath "$GHOST_CONTENT"
+ghost config server.host 0.0.0.0
 sh /bin/migrate_util.sh
 /usr/bin/supervisord
