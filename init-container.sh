9196d7c6dccdc5868707122fd7f785cc35547a07
prashanthmadi <prashanthrmadi@gmail.com>
2017-08-24 13:33:43 -0500

    content directory is already 777

--- init-container.sh
+++ init-container.sh
@@ -4,7 +4,7 @@ gosu node ghost config --db="mysql" --dbhost="$DB_HOST" --dbname="$DB_NAME" --db
 gosu node ghost config paths.contentPath "$GHOST_CONTENT"
 gosu node ghost config database.pool.max 4
 gosu node ghost config database.pool.min 1
-chown -R node "$GHOST_CONTENT"
+# chown -R node "$GHOST_CONTENT"
 echo "current directory"
 echo $PWD
 sh /bin/migrate_util.sh
